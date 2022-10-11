af00bf957561c425c80305bed1d5d84dc2c00b18
ronthetech <rjfrancoisjr@icloud.com>
2022-10-11 10:14:07 -0400

    update min reps to match schema and update disable logic for save button

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -237,7 +237,7 @@ export default function WorkoutForm() {
                         name="workout-reps"
                         id="workout-reps"
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        min={1}
+                        min={0}
                         onChange={(e) => setReps(parseInt(e.target.value))}
                         value={reps}
                       />
@@ -283,12 +283,7 @@ export default function WorkoutForm() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <button
-                    disabled={
-                      !session ||
-                      !title ||
-                      (reps && !load && !minutes) ||
-                      (!reps && !minutes)
-                    }
+                    disabled={!session || !title || (!reps && !minutes)}
                     type="submit"
                     className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-gray-400 disabled:text-gray-300"
                   >
