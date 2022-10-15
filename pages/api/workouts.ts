dfea5c9b73ccb652355a68d597767d9c0242d624
ronthetech <rjfrancoisjr@icloud.com>
2022-10-14 21:28:41 -0400

    feat: add delete functionality

--- pages/api/workouts.ts
+++ pages/api/workouts.ts
@@ -38,6 +38,19 @@ export default async function assetHandler(
           .json({ error: "Error creating workout", success: false });
       }
       break;
+    case "DELETE":
+      try {
+        const deletedWorkout = await prisma.workout.delete({
+          where: {
+            id: body.id,
+          },
+        });
+        res.status(200).json(deletedWorkout);
+      } catch (e) {
+        console.error("Request error", e);
+        res.status(500).json({ error: "Error deleting the workout" });
+      }
+      break;
     default:
       res.setHeader("Allow", ["GET"]);
       res.status(405).end(`Method ${method} Not Allowed`);
