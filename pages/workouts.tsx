a08941c1aa139597bb0515a6a7e598d9b1a4b704
ronthetech <rjfrancoisjr@icloud.com>
2022-10-14 20:42:45 -0400

    refactor and add types

--- pages/workouts.tsx
+++ pages/workouts.tsx
@@ -1,16 +1,17 @@
 import WorkoutDetails from "components/WorkoutDetails";
 import prisma from "lib/prisma";
-import type { NextPage } from "next";
+import type { InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
 
-// const workouts = [
-//   { title: "Pushups", reps: 20, load: 0, minutes: 0 },
-//   { title: "Bench Press", reps: 20, load: 40, minutes: 0 },
-//   { title: "Running", reps: 0, load: 0, minutes: 20 },
-// ];
+export const getServerSideProps = async () => {
+  const workouts = await prisma.workout.findMany();
 
-const Workouts: NextPage = ({ workouts }: any) => {
-  //console.log(typeof workouts);
+  return { props: { workouts: JSON.parse(JSON.stringify(workouts)) } };
+};
+
+export default function Workouts(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
   return (
     <div className="">
       <Head>
@@ -21,33 +22,26 @@ const Workouts: NextPage = ({ workouts }: any) => {
 
       <h1>Workouts</h1>
       <div>
-        {workouts.map((workout: any) => (
-          <WorkoutDetails
-            key={workout.id}
-            title={workout.title}
-            reps={workout.reps}
-            load={workout.load}
-            minutes={workout.minutes}
-            createdAt={workout.createdAt}
-          />
-        ))}
+        {props.workouts.map(
+          (workout: {
+            id: number;
+            title: string;
+            reps: number;
+            load: number;
+            minutes: number;
+            createdAt: Date;
+          }) => (
+            <WorkoutDetails
+              key={workout.id}
+              title={workout.title}
+              reps={workout.reps}
+              load={workout.load}
+              minutes={workout.minutes}
+              createdAt={workout.createdAt}
+            />
+          )
+        )}
       </div>
     </div>
   );
-};
-
-export default Workouts;
-
-export const getServerSideProps = async () => {
-  const workouts = await prisma.workout.findMany();
-
-  return { props: { workouts: JSON.parse(JSON.stringify(workouts)) } };
-};
-
-// const ({workouts}) =>
-//   <ul>
-//    {posts.map(post => (
-//      <li key={post.id}>{post.title}</li>
-//     ))}
-//   </ul>
-// );
+}
