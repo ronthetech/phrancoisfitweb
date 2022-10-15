eaa410cf66159f2cdcf23cd8874510cc39df7bac
ronthetech <rjfrancoisjr@icloud.com>
2022-10-14 20:43:02 -0400

    remove google site verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Phrancois Fit</title>
         <meta name="description" content="Phrancois Fit web app" />
-        <meta
-          name="google-site-verification"
-          content="loPYxtZ3CUNhYYYOKwYi-vLIQ71VUbR0lMdvPB8rXg4"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
