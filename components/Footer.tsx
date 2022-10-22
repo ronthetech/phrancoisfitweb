2499343465f6e3c7c6e74c6cb0ebecad98d06554
ronthetech <rjfrancoisjr@icloud.com>
2022-10-21 23:50:22 -0400

    feat: add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import fitness_tracker_bro from "public/fitness_tracker_bro.svg";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800">
+      {/* <Image
+        alt="Fitness Tracker Bro"
+        src={fitness_tracker_bro}
+        width={500}
+        height={500}
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
+      /> */}
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between text-gray-300">
+          <h1 className="text-3xl font-bold">Phrancois Fit</h1>
+          <a href="https://storyset.com/data">Data illustrations by Storyset</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
