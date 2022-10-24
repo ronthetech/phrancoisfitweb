4c2d7452a1a082bdf51464e263a78fa0ccb5bf65
ronthetech <rjfrancoisjr@icloud.com>
2022-10-23 23:18:30 -0400

    style: change background color

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -218,7 +218,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
           )}
         </Disclosure>
 
-        <header className="bg-white shadow">
+        <header className="bg-gray-300 shadow">
           <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
               Phrancois Fit
