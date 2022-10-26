db880e2984b799eed16bb92acd6339851d921414
ronthetech <rjfrancoisjr@icloud.com>
2022-10-26 10:57:17 -0400

    feat: add auth0 user provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import MainLayout from "components/layouts/MainLayout";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
-function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <UserProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </UserProvider>
   );
 }
 
