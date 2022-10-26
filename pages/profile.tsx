fed746d2e594c8d594a55fd883add29a8cae3041
ronthetech <rjfrancoisjr@icloud.com>
2022-10-26 15:04:39 -0400

    feat: update profile information

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,17 +1,31 @@
+import { useUser } from "@auth0/nextjs-auth0";
 import { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import Head from "next/head";
 
 export default function Profile() {
+  const { user, isLoading } = useUser();
+  if (isLoading)
+    return (
+      <div>
+        <h1 className="text-lg text-indigo-900">LOADING... Please wait</h1>
+      </div>
+    );
   return (
-    <div>
-      <Head>
-        <title>Phrancois Fit</title>
-        <meta name="description" content="Phrancois Fit web app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    user && (
+      <>
+        <Head>
+          <title>Phrancois Fit</title>
+          <meta name="description" content="Phrancois Fit web app" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <h1 className="text-xl font-bold">Profile</h1>
-      <div></div>
-    </div>
+        <h1 className="text-xl font-bold">Profile</h1>
+        <div>
+          <img src={user.picture!} alt={user.name!} />
+          <h1 className="text-lg text-indigo-900">{user?.name}</h1>
+          <p className="text-base text-indigo-800">{user?.email}</p>
+        </div>
+      </>
+    )
   );
 }
