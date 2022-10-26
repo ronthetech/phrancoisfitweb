cd6a7bba8360033a9f88bc5b93ad7361951e815c
ronthetech <rjfrancoisjr@icloud.com>
2022-10-26 10:59:14 -0400

    feat: implement auth0 login and logout

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -8,6 +8,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
 import Footer from "components/Footer";
+import { useUser } from "@auth0/nextjs-auth0";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -16,12 +17,12 @@ interface MainLayoutProps {
 const navigation = [
   { name: "Home", href: "/", current: true },
   { name: "Workouts", href: "/workouts", current: false },
-  { name: "Add a Workout", href: "/addworkout", current: false },
+  { name: "Add Workout", href: "/addworkout", current: false },
 ];
 const userNavigation = [
-  { name: "Your Profile", href: "#" },
+  { name: "Your Profile", href: "/profile" },
   { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Sign out", href: "/api/auth/logout" },
 ];
 
 function classNames(...classes: string[]) {
@@ -30,6 +31,7 @@ function classNames(...classes: string[]) {
 
 export default function MainLayout({ children }: MainLayoutProps) {
   const router = useRouter();
+  const { user, isLoading } = useUser();
 
   return (
     <>
@@ -83,55 +85,63 @@ export default function MainLayout({ children }: MainLayoutProps) {
                       </button>
 
                       {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="sr-only">Open user menu</span>
-                            <UserIcon className="h-8 w-8 rounded-full" />
-                            {/* <Image
+                      {user ? (
+                        <Menu as="div" className="relative ml-3">
+                          <div>
+                            <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                              <span className="sr-only">Open user menu</span>
+                              <UserIcon className="h-8 w-8 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" />
+                              {/* <Image
                                 className="h-8 w-8 rounded-full"
                                 src={user.image!}
                                 alt={user.name!}
                                 height={32}
                                 width={32}
                               /> */}
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
+                            </Menu.Button>
+                          </div>
+                          <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                          >
+                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                              {userNavigation.map((item) => (
+                                <Menu.Item key={item.name}>
+                                  {({ active }) => (
+                                    <a
+                                      href={item.href}
+                                      className={classNames(
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-sm text-gray-700"
+                                      )}
+                                    >
+                                      {item.name}
+                                    </a>
+                                  )}
+                                </Menu.Item>
+                              ))}
+                            </Menu.Items>
+                          </Transition>
+                        </Menu>
+                      ) : (
+                        // eslint-disable-next-line @next/next/no-html-link-for-pages
+                        <a
+                          className="text-gray-300 text:bg-gray-500"
+                          href="/api/auth/login"
                         >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                    onClick={(e) => {
-                                      e.preventDefault();
-                                    }}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
+                          Login
+                        </a>
+                      )}
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
                     {/* Mobile menu button */}
+
                     <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
@@ -172,19 +182,22 @@ export default function MainLayout({ children }: MainLayoutProps) {
                     </Disclosure.Button>
                   ))}
                 </div>
-                <div className="border-t border-gray-700 pt-4 pb-3">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <UserIcon className="h-8 w-8 rounded-full" />
-                      {/* <Image
+
+                {user ? (
+                  <div className="border-t border-gray-700 pt-4 pb-3">
+                    <div className="flex items-center px-5">
+                      <div className="flex-shrink-0">
+                        <UserIcon className="h-8 w-8 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" />
+                        {/* <Image
                           className="h-10 w-10 rounded-full"
                           src={user.image!}
                           alt={user.name!}
                           height={32}
                           width={32}
                         /> */}
-                    </div>
-                    {/* <div className="ml-3">
+                      </div>
+
+                      {/* <div className="ml-3">
                       <div className="text-base font-medium leading-none text-white">
                         {user.name}
                       </div>
@@ -192,27 +205,37 @@ export default function MainLayout({ children }: MainLayoutProps) {
                         {user.email}
                       </div>
                     </div> */}
-                    <button
-                      type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      <button
+                        type="button"
+                        className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+
+                    <div className="mt-3 space-y-1 px-2">
+                      {userNavigation.map((item) => (
+                        <Disclosure.Button
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      ))}
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  // eslint-disable-next-line @next/next/no-html-link-for-pages
+                  <a
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                    href="/api/auth/login"
+                  >
+                    Login
+                  </a>
+                )}
               </Disclosure.Panel>
             </>
           )}
