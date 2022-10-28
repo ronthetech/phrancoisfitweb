e4ae4f90fe1f4d768c19e7baa0d2ae31c62e56c6
ronthetech <rjfrancoisjr@icloud.com>
2022-10-28 19:44:26 -0400

    chore: remove unused divs

--- components/WorkoutDetails.tsx
+++ components/WorkoutDetails.tsx
@@ -48,42 +48,38 @@ const WorkoutDetails = ({
 
   return (
     <div className="bg-indigo-200 rounded-lg text-left shadow-lg transition-all sm:my-8 my-6 sm:w-full sm:max-w-xl p-6 mx-auto">
-      <div>
+      <>
         <h2 className="text-xl font-medium leading-6 text-gray-900">{title}</h2>
-      </div>
-      <div>
-        <div>
-          {reps ? (
-            <p className="mt-1 text-lg text-gray-800">
-              <strong>Reps: {reps}</strong>
-            </p>
-          ) : (
-            <p></p>
-          )}
-          <p className="mt-1 text-lg text-gray-800">
-            <strong>Load (lbs): {load}</strong>
-          </p>
-          {minutes ? (
-            <p className="mt-1 text-lg text-gray-800">
-              <strong>Length (mins): {minutes}</strong>
-            </p>
-          ) : (
-            <p></p>
-          )}
-        </div>
-        <p className="mt-1 text-lg text-gray-800 font-bold">
-          {formatDistanceToNow(new Date(createdAt), { addSuffix: true })}
+      </>
+      {reps ? (
+        <p className="mt-1 text-lg text-gray-800">
+          <strong>Reps: {reps}</strong>
         </p>
-        <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button
-            disabled={!user}
-            type="button"
-            className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm disabled:bg-gray-400 disabled:text-gray-300"
-            onClick={handleClick}
-          >
-            Delete
-          </button>
-        </div>
+      ) : (
+        <p></p>
+      )}
+      <p className="mt-1 text-lg text-gray-800">
+        <strong>Load (lbs): {load}</strong>
+      </p>
+      {minutes ? (
+        <p className="mt-1 text-lg text-gray-800">
+          <strong>Length (mins): {minutes}</strong>
+        </p>
+      ) : (
+        <p></p>
+      )}
+      <p className="mt-1 text-lg text-gray-800 font-bold">
+        {formatDistanceToNow(new Date(createdAt), { addSuffix: true })}
+      </p>
+      <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+        <button
+          disabled={!user}
+          type="button"
+          className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm disabled:bg-gray-400 disabled:text-gray-300"
+          onClick={handleClick}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
