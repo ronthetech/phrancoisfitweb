cf530103a211918baf4bc336a4ba8bca1997e09e
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-11-22 13:58:18 -0500

    update delete button

--- components/WorkoutDetails.tsx
+++ components/WorkoutDetails.tsx
@@ -77,11 +77,11 @@ const WorkoutDetails = ({
       <p className="mt-1 text-lg text-gray-800 font-bold">
         {formatDistanceToNow(new Date(createdAt), { addSuffix: true })}
       </p>
-      <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+      <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 text-lg">
         <button
           disabled={!user}
           type="button"
-          className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm disabled:bg-gray-400 disabled:text-gray-300"
+          className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-lg text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-base disabled:bg-gray-400 disabled:text-gray-300"
           onClick={handleClick}>
           Delete
           <Image
